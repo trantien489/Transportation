67387c7e2ff24149a2980ea2ca54c4dcde3c3681
Tien Tran <ttran279@dxc.com>
2019-11-17 22:46:57 +0700

    Update

--- WebAPI/Transportation/Startup.cs
+++ WebAPI/Transportation/Startup.cs
@@ -1,9 +1,17 @@
-﻿using Infrastructure.EF.Context;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.EF.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Transportation
 {
@@ -19,7 +27,10 @@ namespace Transportation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                                        b => b.MigrationsAssembly("Infrastructure.EF")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
